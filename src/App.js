c207784c3b172983e8c7e592a864436dbfa8db0c
JIHO SONG <thdwlgh44@naver.com>
2020-12-27 21:15:55 +0900

    #5 CONCLUSIONS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import axios from "axios";
-import Movie from "./Movie";
+import axios from 'axios';
+import Movie from './Movie';
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -23,22 +24,30 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map(moive => (
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-            />
-          ))}
-      </div >
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+            <div className="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))}
+            </div>
+          )}
+      </section>
     );
   }
 }
+
 
 export default App;
